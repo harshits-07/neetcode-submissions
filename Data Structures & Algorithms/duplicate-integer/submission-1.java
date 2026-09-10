bbced02eb37528436391b6f7a73248aed66c2699
Harshit Sharma <161496390+harshits-07@users.noreply.github.com>
2026-09-11 04:25:23 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int duplicate = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    duplicate++;
+                    break;
+                }
+            }
+        }
+        if(duplicate>0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+ 
+    }
+}
---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int ln = nums.length;
+        int low=0,mid=0,high=ln-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                nums[mid]=nums[low];
+                nums[low]=0;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                nums[mid]=nums[high];
+                nums[high]=2;
+                high--;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+        }
+    }
+}
